3fadd93bb04c82c4eed23d9d3fa1c89f2c68ede0
Rob Monhemius <robmonhemius@gmail.com>
2022-01-24 19:58:15 +0100

    created placeholder

--- src/edit.js
+++ src/edit.js
@@ -1,16 +1,23 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
-import { TextControl } from '@wordpress/components';
+import { Placeholder, TextControl } from '@wordpress/components';
 import './editor.scss';
 
-export default function Edit( { attributes, setAttributes} ) {
+export default function Edit( { attributes, className, setAttributes} ) {
 	return (
 		<div {...useBlockProps()}>
+			<Placeholder
+				label = { __( 'Gutenpride Block', 'gutenpride' ) }
+				instructions = { __( 'Add your message', 'gutenpride' ) }
+			>
+
 			<TextControl 
 				label = { __( 'Message', 'gutenpride' ) }
 				value = { attributes.message }
 				onChange = { ( val ) => setAttributes( { message: val } ) }
 			/>
+
+			</Placeholder>
 		</div>
 	);
 }
