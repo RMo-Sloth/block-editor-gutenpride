b73efca41a1b986bc8c34f016e1caeee64ca880b
Rob Monhemius <robmonhemius@gmail.com>
2022-01-24 20:48:34 +0100

    block editor preview

--- src/edit.js
+++ src/edit.js
@@ -3,21 +3,29 @@ import { useBlockProps } from '@wordpress/block-editor';
 import { Placeholder, TextControl } from '@wordpress/components';
 import './editor.scss';
 
-export default function Edit( { attributes, className, setAttributes} ) {
-	return (
-		<div {...useBlockProps()}>
+export default function Edit( { attributes, className, setAttributes, isSelected } ) {
+
+	let content;
+	if( !isSelected && attributes.message ) {
+		content = <div>{ attributes.message }</div>;
+	} else {
+		content = (
 			<Placeholder
 				label = { __( 'Gutenpride Block', 'gutenpride' ) }
 				instructions = { __( 'Add your message', 'gutenpride' ) }
 			>
-
-			<TextControl 
-				label = { __( 'Message', 'gutenpride' ) }
-				value = { attributes.message }
-				onChange = { ( val ) => setAttributes( { message: val } ) }
-			/>
-
+				<TextControl 
+					label = { __( 'Message', 'gutenpride' ) }
+					value = { attributes.message }
+					onChange = { ( val ) => setAttributes( { message: val } ) }
+				/>
 			</Placeholder>
+		);
+	}
+
+	return (
+		<div {...useBlockProps()}>
+			{ content }
 		</div>
 	);
 }
